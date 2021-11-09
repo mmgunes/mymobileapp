c7a3ed096ba6d05f01b3d6124340e90b2b7cc606
mmgunes <yerisaretlerim1@gmail.com>
2021-11-09 17:29:54 +0300

    flex ile sayfayı bölümleme

--- src/App.js
+++ src/App.js
@@ -10,17 +10,28 @@ import React from 'react';
 
 import {Text, View} from 'react-native';
 
-import { styles } from './style';
-
+import {styles} from './style';
 
 export default App = () => {
   return (
     <View style={styles.container}>
-     {/*Ortalama / <Text style={{textAlign: 'center'}}>MERHABA REACT NATİVE</Text> */}
-    <Text style={styles.textStyle}>YAZI 1 </Text>
-    <Text>YAZI 2 </Text>
-    <Text style={styles.textStyle}>YAZI 3 </Text>
-    <Text>YAZI 4 </Text>
+      {/*Ortalama / <Text style={{textAlign: 'center'}}>MERHABA REACT NATİVE</Text> */}
+      <View style={styles.header}>
+        <Text style={styles.textStyle}>HEADER KISMI</Text>
+      </View>
+
+      <View style={styles.main}>
+        <View style={styles.side}>
+          <Text style={styles.textStyle}> SİDE BAR</Text>
+        </View>
+        <View style={styles.content}>
+          <Text style={styles.textStyle}>MAİN KISMI</Text>
+        </View>
+      </View>
+
+      <View style={styles.footer}>
+        <Text style={styles.textStyle}>FOOTER KISMI</Text>
+      </View>
     </View>
   );
 };
--- src/style.js
+++ src/style.js
@@ -10,8 +10,43 @@ export const styles = StyleSheet.create({
     flexDirection: 'column', // 'column' Varsayılanıdır flexDirection:'column'
     backgroundColor: 'purple',
   },
-  
+
   textStyle: {
-      color: '#fff',
-  }
+    fontSize: 24,
+    color: '#000',
+    textAlign: 'center',
+  },
+
+  header: {
+    flex: 2,
+    alignContent: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ff0',
+  },
+  main: {
+    flexDirection: 'row',
+    flex: 5,
+    alignContent: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#0f0',
+  },
+  side: {
+    flex: 2,
+    alignContent: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'purple',
+  },
+  content: {
+    flex: 6,
+    alignContent: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'green',
+  },
+
+  footer: {
+    flex: 1,
+    alignContent: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#00f',
+  },
 });
