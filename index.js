c3e8bbc1864002653d893c6add0bcfecc6a22349
mmgunes <yerisaretlerim1@gmail.com>
2021-11-11 16:41:17 +0300

    sayac adı verildi

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 import App2 from './src/App2';
-import App3 from './src/App3';
+import App3 from './src/sayac';
 AppRegistry.registerComponent(appName, () => App3);
