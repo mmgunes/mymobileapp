8268dff57e8bc203f861033ed282747b29008090
mmgunes <yerisaretlerim1@gmail.com>
2021-11-12 16:42:21 +0300

    ikon kullanma ve harici paket yükleme

--- src/HariciPaketIcon.js
+++ src/HariciPaketIcon.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+
+import IconFa from 'react-native-vector-icons/FontAwesome';
+
 export default HariciPaketIcon = () => {
   return (
     <View
       style={{flex: 1, backgroundColor:'yellow', alignItems: 'center', justifyContent: 'center'}}>
 
+
+        <IconFa name="car" size={50} color="black" />
         <Text>Harici Paket Yükleme </Text>
+        {/* Komutu çalıştır: npx react-native link react-native-vector-icons 
+        ikonların doğru yüklenmesi için gerekli  */}
 
 
     </View>
