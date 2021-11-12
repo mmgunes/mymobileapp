a0e8548aab5407a816445535830402818b76d851
mmgunes <yerisaretlerim1@gmail.com>
2021-11-12 15:33:31 +0300

    rn npm ile vector-icons paketi ekleme

--- index.js
+++ index.js
@@ -9,4 +9,6 @@ import App2 from './src/App2';
 import App3 from './src/sayac';
 import TextInputKullan from './src/TextInputKullan';
 import Login from './src/Login'
-AppRegistry.registerComponent(appName, () => Login);
+import HariciPaketIcon from './src/HariciPaketIcon'
+
+AppRegistry.registerComponent(appName, () => HariciPaketIcon);
---
+++ src/HariciPaketIcon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+export default HariciPaketIcon = () => {
+  return (
+    <View
+      style={{flex: 1, backgroundColor:'yellow', alignItems: 'center', justifyContent: 'center'}}>
+
+        <Text>Harici Paket Yükleme </Text>
+
+
+    </View>
+  );
+};
